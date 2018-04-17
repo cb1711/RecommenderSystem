02d96b32b20b2decdb576c2d282c8e0d26711648
cb1711 <chaitanya.bhatia.cse15@iitbhu.ac.in>
2018-04-17 15:23:25 +0530

    src/main.cpp
    Minor fixes

--- src/main.cpp
+++ src/main.cpp
@@ -11,28 +11,43 @@ int main(int argc,char* argv){
 	int numUsers,numItems,numRatings;
 	cin >> numUsers>>numItems>>numRatings;
 	//take input
-	int *csr_users = new int[numRatings + 1];
-	int *items = new int[numItems];
-	/*Format conversion*/
-	int prevRow = 0;
+	int *csr_users = new int[numUsers+1];
+	int *items = new int[numRatings];
+	int *csr_items=new int[numItems+1];
+	int *users=new int[numRatings];
+    /*Format conversion*/
+	int prevItem = 0;
 	csr_users[0] = 0;
+	int prevUser= 0;
 	items[0]=0;
 	for (int i = 0; i<numRatings; i++){
-		cin >> csr_users[i];
-		csr_users[i]--;
-		int curr_row;
-		cin >> curr_row;
-		curr_row--;
-		while (prevRow<curr_row){
-			prevRow++;
-			items[prevRow] = i;
+		cin >> items[i];
+		items[i]--;
+		int curr_user;
+		cin >> curr_user;
+		curr_user--;
+		cin >> users[i];
+		users[i]--;
+		int curr_item;
+		cin >> curr_item;
+		curr_item--;
+		while (prevItem<curr_item){
+			prevItem++;
+			csr_items[prevItem] = i;
+		}
+		while (prevUser<curr_user){
+            prevUser++;
+            csr_users[prevUser] = i;
 		}
 	}
-	while (prevRow<numRows){
-		prevRow++;
-		items[prevRow]=numRatings;
+	while (prevItem<=numItems){
+		prevItem++;
+		csr_items[prevItem]=numRatings;
 	}
-
+	while (prevUser<=numUsers){
+		prevUser++;
+		csr_users[prevUser]=numRatings;
+	}
 	//setup mpi
 	int numProcs,rank;
 	MPI_Init(&argc,&argv);
@@ -40,29 +55,29 @@ int main(int argc,char* argv){
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
 	int *all_items,*all_users;
-	int *allotted_items,*allotted_users;
+	int *alloted_items,*alloted_users;
 
 	//Divide work
 	if(rank==MASTER){ //Use better method for alloting
-        	*all_items=new int[numItems];
-        	*all_users=new int[numUsers];
-        	for(int i=0;i<numItems;i++){
-            		allotted_items[i]=i;
-        	}
-        	for(int i=0;i<numUsers;i++){
-        	    allotted_items[i]=i;
-        	}
-    	}	
-    	allotted_items=new int[numItems/numProcs + 1];
-    	allotted_users=new int[numUsers/numProcs + 1];
-    	MPI_Scatter(all_users,numUsers/numProcs+1,MPI_INT,alloted_users,numUsers/numProcs+1,MPI_INT,MASTER,MPI_COMM_WORLD);
-    	MPI_Scatter(all_items,numItems/numProcs+1,MPI_INT,alloted_items,numItems/numProcs+1,MPI_INT,MASTER,MPI_COMM_WORLD);
-    	float **fi,**fu;
-    	int count_user,count_item;
-    	count_user=(rank==numProcs-1)?(numUsers-(numProcs-1)*(numUsers/numProcs+1)):numUsers/numProcs+1;
-    	count_item=(rank==numProcs-1)?(numItems-(numProcs-1)*(numItems/numProcs+1)):numItems/numProcs+1;
-    	//Call ocular
-    	ocular(numItems,numUsers,csr_item,users,csr_user,items,fi,fu,allotted_users,allotted_items,count_user,count_item);
+        *all_items=new int[numItems];
+        *all_users=new int[numUsers];
+        for(int i=0;i<numItems;i++){
+            alloted_items[i]=i;
+        }
+        for(int i=0;i<numUsers;i++){
+            alloted_items[i]=i;
+        }
+    }
+    alloted_items=new int[numItems/numProcs + 1];
+    alloted_users=new int[numUsers/numProcs + 1];
+    MPI_Scatter(all_users,numUsers/numProcs+1,MPI_INT,alloted_users,numUsers/numProcs+1,MPI_INT,MASTER,MPI_COMM_WORLD);
+    MPI_Scatter(all_items,numItems/numProcs+1,MPI_INT,alloted_items,numItems/numProcs+1,MPI_INT,MASTER,MPI_COMM_WORLD);
+    float **fi,**fu;
+    int count_user,count_item;
+    count_user=(rank==numProcs-1)?(numUsers-(numProcs-1)*(numUsers/numProcs+1)):numUsers/numProcs+1;
+    count_item=(rank==numProcs-1)?(numItems-(numProcs-1)*(numItems/numProcs+1)):numItems/numProcs+1;
+    //Call ocular
+    ocular(numItems,numUsers,csr_item,users,csr_user,items,fi,fu,alloted_users,alloted_items,count_user,count_item);
 
 	MPI_Finalize();
 	return 0;
