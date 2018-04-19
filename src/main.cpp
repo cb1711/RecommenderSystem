266103044017d0f99a083a3090bd4da3c5233566
Pranjal Jain <pranjal.jain.cse15@iitbhu.ac.in>
2018-04-19 13:33:13 +0530

    Cleaned up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,122 +8,138 @@
 using namespace std;
 
 int main(int argc,char* argv[]){
-    int numProcs,rank;
-    MPI_Init(&argc,&argv);
-	MPI_Comm_size(MPI_COMM_WORLD,&numProcs);//Set up different communicators when work broadcast has to be removed
+	int numProcs,rank;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&numProcs);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    int numUsers,numItems,numRatings;
-    int tempTransfer[]={0,0,0};
-    if(rank==MASTER){
-        cin >> numItems>>numUsers>>numRatings;
-        tempTransfer[0]=numItems;
-        tempTransfer[1]=numUsers;
-        tempTransfer[2]=numRatings;
-    }
-    MPI_Bcast(tempTransfer,3,MPI_INT,MASTER,MPI_COMM_WORLD);
-    numItems=tempTransfer[0];
-    numUsers=tempTransfer[1];
-    numRatings=tempTransfer[2];
-    int *csr_users = new int[numUsers+1];
+	int tempTransfer[]={0,0,0};
+	if(rank==MASTER){
+		cin >> tempTransfer[0] >> tempTransfer[1] >> tempTransfer[2];
+	}
+	MPI_Bcast(tempTransfer,3,MPI_INT,MASTER,MPI_COMM_WORLD);
+	int numItems=tempTransfer[0];
+	int numUsers=tempTransfer[1];
+	int numRatings=tempTransfer[2];
+	int *csr_users = new int[numUsers+1];
 	int *items = new int[numRatings];
 	int *csr_items=new int[numItems+1];
 	int *users=new int[numRatings];
-    if(rank==MASTER){
-
-        //take input
-
-        /*Format conversion*/
-        int prevItem = 0;
-        csr_users[0] = 0;
-        int prevUser= 0;
-        items[0]=0;
-        for(int i=0;i<numRatings;i++){
-
-            int curr_item;
-            cin >> curr_item;
-            curr_item--;
-            cin >> users[i];
-            users[i]--;
-            while (prevItem<curr_item){
-                prevItem++;
-                csr_items[prevItem] = i;
-            }
-        }
-
-
-        for (int i = 0; i<numRatings; i++){
-            cin >> items[i];
-            items[i]--;
-            int curr_user;
-            cin >> curr_user;
-            curr_user--;
-            while (prevUser<curr_user){
-                prevUser++;
-                csr_users[prevUser] = i;
-            }
-        }
-
-        while (prevItem<=numItems){
-            prevItem++;
-            csr_items[prevItem]=numRatings;
-        }
-        while (prevUser<=numUsers){
-            prevUser++;
-            csr_users[prevUser]=numRatings;
-        }
-
+	#pragma omp parallel
+	{
+		#pragma omp for
+		for(int i = 0; i < numUsers+1; i++)
+		{
+			csr_users[i] = 0;
+		}
+		#pragma omp for
+		for(int i = 0; i < numItems+1; i++)
+		{
+			csr_items[i] = 0;
+		}
+		#pragma omp for
+		for(int i = 0; i < numRatings; i++)
+		{
+			items[i] = users[i] = 0;
+		}
 	}
+
+	if(rank==MASTER){
+		for(int i = 0; i < numRatings; i++){
+			int item, user;
+			cin >> item >> user;
+			user--;
+			users[i] = user;
+			csr_items[item]++;
+		}
+		for(int i = 1; i <= numItems; i++){
+			csr_items[i] += csr_items[i-1];
+		}
+
+		for(int i = 0; i < numRatings; i++){
+			int user, item;
+			cin >> user >> item;
+			item--;
+			items[i] = item;
+			csr_users[user]++;
+		}
+		for(int i = 1; i <= numUsers; i++){
+			csr_users[i] += csr_users[i-1];
+		}
+	}
+
 	//setup mpi
-
 	MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Bcast(users,numRatings,MPI_INT,MASTER,MPI_COMM_WORLD);
-    MPI_Bcast(items,numRatings,MPI_INT,MASTER,MPI_COMM_WORLD);
-    MPI_Bcast(csr_users,(numUsers+1),MPI_INT,MASTER,MPI_COMM_WORLD);
-    MPI_Bcast(csr_items,(numItems+1),MPI_INT,MASTER,MPI_COMM_WORLD);
+	MPI_Bcast(users,numRatings,MPI_INT,MASTER,MPI_COMM_WORLD);
+	MPI_Bcast(items,numRatings,MPI_INT,MASTER,MPI_COMM_WORLD);
+	MPI_Bcast(csr_users,numUsers+1,MPI_INT,MASTER,MPI_COMM_WORLD);
+	MPI_Bcast(csr_items,numItems+1,MPI_INT,MASTER,MPI_COMM_WORLD);
 
-	int *all_items,*all_users;
-	int *alloted_items,*alloted_users;
+	int *all_items;
+	int *all_users;
+	int process_items = numItems/numProcs+(numItems%numProcs<rank);
+	int process_users = numUsers/numProcs+(numUsers%numProcs<rank);
+	int *alloted_items=new int[process_items];
+	int *alloted_users=new int[process_users];
 
 	//Divide work
-	if(rank==MASTER){ //Use better method for alloting
-        all_items=new int[numItems];
-        all_users=new int[numUsers];
-        for(int i=0;i<numItems;i++){
-            all_items[i]=i;
-        }
-        for(int i=0;i<numUsers;i++){
-            all_users[i]=i;
-        }
-    }
-    alloted_items=new int[numItems/numProcs + 1];
-    alloted_users=new int[numUsers/numProcs + 1];
-    MPI_Scatter(all_users,numUsers/numProcs+1,MPI_INT,alloted_users,numUsers/numProcs+1,MPI_INT,MASTER,MPI_COMM_WORLD);
-    MPI_Scatter(all_items,numItems/numProcs+1,MPI_INT,alloted_items,numItems/numProcs+1,MPI_INT,MASTER,MPI_COMM_WORLD);
-    float **fi,**fu;
-    int count_user,count_item;
-    count_user=(rank==numProcs-1)?(numUsers-(numProcs-1)*(numUsers/numProcs+1)):numUsers/numProcs+1;
-    count_item=(rank==numProcs-1)?(numItems-(numProcs-1)*(numItems/numProcs+1)):numItems/numProcs+1;
-    //Call ocular
-    fi = new float*[((numItems/numProcs)+1)*numProcs];
-	fu = new float*[((numUsers/numProcs)+1)*numProcs];
-	for(int i = 0; i < ((numItems/numProcs)+1)*numProcs; i++){
-		fi[i] = new float[CLUSTERS];
-	}
-	for(int i = 0; i < ((numUsers/numProcs)+1)*numProcs; i++){
-		fu[i] = new float[CLUSTERS];
-	}
-    ocular(numItems,numUsers,csr_items,users,csr_users,items,fi,fu,alloted_users,alloted_items,count_user,count_item);
+	int* sendcounts_item;
+	int* displs_item;
+	int* sendcounts_user;
+	int* displs_user;
 
+	if(rank==MASTER){
+		sendcounts_item=new int[numProcs];
+		displs_item=new int[numProcs];
+		for(int i = 0; i < numProcs; i++){
+			sendcounts_item[i] = numItems/numProcs+(numItems%numProcs<i);
+			if(i==0)displs_item[i] = 0;
+			else displs_item[i] = displs_item[i-1] + sendcounts_item[i-1];
+		}
+		sendcounts_user=new int[numProcs];
+		displs_user=new int[numProcs];
+		for(int i = 0; i < numProcs; i++){
+			sendcounts_user[i] = numUsers/numProcs+(numUsers%numProcs<i);
+			if(i==0)displs_user[i] = 0;
+			else displs_user[i] = displs_user[i-1] + sendcounts_user[i-1];
+		}
+		all_items = new int[numItems];
+		all_users = new int[numUsers];
+		for(int i=0;i<numItems;i++){
+			all_items[i]=i;
+		}
+		for(int i=0;i<numUsers;i++){
+			all_users[i]=i;
+		}
+	}
+
+	MPI_Scatterv(all_items,sendcounts_item,displs_item,MPI_INT,alloted_items,process_items,MPI_INT,MASTER,MPI_COMM_WORLD);
+	MPI_Scatterv(all_users,sendcounts_user,displs_user,MPI_INT,alloted_users,process_users,MPI_INT,MASTER,MPI_COMM_WORLD);
+
+    //Call ocular
+	float **fi,**fu;
+	float *item_data = new float[numItems*CLUSTERS];
+	float *user_data = new float[numUsers*CLUSTERS];
+
+	fi = new float*[numItems];
+	fu = new float*[numUsers];
+	for(int i = 0; i < numItems; i++){
+		fi[i] = &(item_data[i*CLUSTERS]);
+	}
+	for(int i = 0; i < numUsers; i++){
+		fu[i] = &(user_data[i*CLUSTERS]);
+	}
+	ocular(numItems,numUsers,csr_items,users,csr_users,items,fi,fu,alloted_items,alloted_users,process_items,process_users,sendcounts_item,sendcounts_user,displs_item,displs_user);
 	MPI_Finalize();
+
 	int user_id,item_id;
 	cout<<"Enter the user and item"<<endl;
-	//while(){
-		//cin>>user_id>>item_id;
-		user_id=0;
-		item_id=2;
-
+	while(1){
+		cin>>user_id>>item_id;
+		if(user_id<1 or user_id>numUsers or item_id<0 or item_id>numItems)break;
+		user_id--;
+		item_id--;
 		float x=innerProduct(fi[item_id],fu[user_id],CLUSTERS);
-		cout<<1-pow(M_E,-x)<<endl;
-	//}
+		cout<<100*(1-pow(M_E,-x))<<endl;
+	}
 	return 0;
 }
